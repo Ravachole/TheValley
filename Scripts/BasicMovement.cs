c8a84a8ae69f6a9b05707bae8a4d390f818fe1e2
Ravachole <133241545+Ravachole@users.noreply.github.com>
2024-08-21 13:05:13 +0200

    Mouse handling

--- Scripts/BasicMovement.cs
+++ Scripts/BasicMovement.cs
@@ -3,12 +3,49 @@ using System;
 
 public partial class BasicMovement : CharacterBody3D
 {
-	public const float Speed = 15.0f;
+	// Physics variables => MOVE TO GLOBAL CONST
+	public const float Speed = 10.0f;
 	public const float JumpVelocity = 4.5f;
+	// End Physics variables
+	// Mouse Variables for player 
+	public const float lookAngle = 90.0f;
+	public const float mouseSensitivity = 0.1f;
+	public Vector2 mouseDelta = new Vector2();
+	// End Mouse variables for player
 
-	public override void _PhysicsProcess(double delta)
+	// References
+	public Camera3D camera;
+
+    // Loop principale / Ready
+
+    public override void _Ready()
+    {
+        base._Ready();
+		// getCamera
+		camera = GetNode("PlayerCamera") as Camera3D;
+    }
+
+    // Handling of Camera movement with mouse
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion eventMouseMotion) 
+		{
+			// Get mouse mvmt
+			mouseDelta = eventMouseMotion.Relative;
+		}
+    }
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
+
+		// Rotation set on camera (Y)
+		//camera.RotationDegrees -= new Vector3(Mathf.RadToDeg(mouseDelta.Y), 0, 0) * mouseSensitivity;
+		camera.RotationDegrees = new Vector3(Mathf.Clamp(camera.RotationDegrees.X, -lookAngle, lookAngle), 
+		camera.RotationDegrees.Y, camera.RotationDegrees.Z);
+		// Rotation set on player (X)
+		RotationDegrees -= new Vector3(0, Mathf.RadToDeg(mouseDelta.X), 0) * mouseSensitivity;
+		mouseDelta = new Vector2();
 
 		// Add the gravity.
 		if (!IsOnFloor())
